783f9369e352cdb52aa1f253d5763ed7b578baff
Mark Walsh <S00181377@mail.itsligo.ie>
2019-04-01 23:54:38 +0100

    First Commit

---
+++ TwitchApp/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Threading;
+using TwitchLib.Client;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+
+namespace TwitchApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        ObservableCollection<string> ChatList = new ObservableCollection<string>();
+
+        TwitchClient client;
+
+        public MainWindow()
+        {
+            Bot bot = new Bot();
+            InitializeComponent();
+            ChatBox.ItemsSource = ChatList;
+        }
+
+        private void ChatBox_DragEnter(object sender, DragEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Search_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectionCredentials credentials = new ConnectionCredentials("ToastedToastie", "v1vyufbf7ljw0bsgozva0zym3zzcov");
+
+            string input = SearchBox.Text;
+
+
+            client = new TwitchClient();
+            client.Initialize(credentials, input);
+            client.JoinRoom(input, null, false);
+
+            client.OnConnected += Client_OnConnected;
+            //client.OnJoinedChannel += Client_OnJoinedChannel;
+            client.OnMessageReceived += Client_OnMessageReceived;
+            //client.OnNewSubscriber += Client_OnNewSubscriber;
+
+            client.Connect();
+
+
+
+        }
+        private void Client_OnConnected(object sender, OnConnectedArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                ChatList.Add($"Connected to {e.AutoJoinChannel}\n");
+            }
+            ));
+        }
+
+        private void Client_OnJoinedChannel(object sender, OnJoinedChannelArgs e)
+        {
+            ChatList.Add($"Connected to {e.Channel.ToString()}'s chat room!");
+        }
+
+        private void Client_OnMessageReceived(object sender, OnMessageReceivedArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                ChatList.Add($"{e.ChatMessage.Username}: {e.ChatMessage.Message}");
+                xx.ScrollToBottom();
+            }), DispatcherPriority.Background);
+        }
+
+        //private void Client_OnNewSubscriber(object sender, OnNewSubscriberArgs e)
+        //{
+        //    TestingBox.Text = ($"{e.Subscriber.SystemMessageParsed}");
+        //}
+
+        //private void Client_OnResubcriber(object sender, OnReSubscriberArgs e)
+        //{
+        //    TestingBox.Text = ($"{e.ReSubscriber.SystemMessageParsed}");
+        //}
+    }
+}
---
+++ TwitchApp/TwitchChatBot.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using 
+using TwitchLib.Api.Helix.Models.Users;
+using TwitchLib.Api.V5.Models.Subscriptions;
+
+namespace Example
+{
+    class Program
+    {
+        private static TwitchAPI api;
+
+        private void Main()
+        {
+            api = new TwitchAPI();
+            api.Settings.ClientId = "client_id";
+            api.Settings.AccessToken = "access_token";
+        }
+
+        private async Task ExampleCallsAsync()
+        {
+            //Checks subscription for a specific user and the channel specified.
+            Subscription subscription = await api.V5.Channels.CheckChannelSubscriptionByUserAsync("channel_id", "user_id");
+
+            //Gets a list of all the subscritions of the specified channel.
+            List<Subscription> allSubscriptions = await api.V5.Channels.GetAllSubscribersAsync("channel_id");
+
+            //Get channels a specified user follows.
+            GetUsersFollowsResponse userFollows = await api.Helix.Users.GetUsersFollowsAsync("user_id");
+
+            //Get Spedicified Channel Follows
+            var channelFollowers = await api.V5.Channels.GetChannelFollowersAsync("channel_id");
+
+            //Return bool if channel is online/offline.
+            bool isStreaming = await api.V5.Streams.BroadcasterOnlineAsync("channel_id");
+
+            //Update Channel Title/Game
+            await api.V5.Channels.UpdateChannelAsync("channel_id", "New stream title", "Stronghold Crusader");
+        }
+    }
+}
